00f4c69d519c8ffdb6666cac74fa6437dae31755
harshabhat91 <harshabhat91@gmail.com>
2021-06-19 21:07:18 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -6,15 +6,18 @@ header('Content-Type: application/json');
 #	];
 while(true) {
     if( time() % 5 == 0 ) { // get time in seconds and check if it is multiple of 5
-        $cbStatus = rand(0, 10);
-		$raStatus = rand(0, 10);
+        $cbStatus = rand(0, 1);
+		if ($cbStatus == 1) {
+			$raStatus = 0;
+			else {
+				$raStatus = 1;
+			}
+		}
         break; // break the loop
     } else {
         sleep(1);
     }
 }
-
-
 
 $arr = array('a' => $cbStatus, 'b' => $raStatus);
 echo json_encode($arr);
