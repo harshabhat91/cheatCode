e4e1d04bf70fd710166f77328e169bc9e3e6871b
harshabhat91 <harshabhat91@gmail.com>
2021-06-19 20:51:55 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/json');
 $data = [
-	'status' => (time() % 10000) * 3
+	'cbStatus' => (time() % 10000) * 3
+	'raStatus' => (time() % 10000) * 3
 	];
 echo json_encode($data);
